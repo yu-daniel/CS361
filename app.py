b630094128ef12a739e58c76db61fd0af8b85bbd
Daniel Yu <liquid.d9yu@gmail.com>
2021-05-09 16:50:41 -0700

    Microservice: improved the parsing of text that include unicode characters, new line characters, and foreign language characters.

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 
 
 def scraper(route, method):
+    foreign_lang = False
     try:
         my_url = "https://en.wikipedia.org/wiki/"
 
@@ -17,30 +18,47 @@ def scraper(route, method):
             # Parameters for the GET requests was referenced from https://www.mediawiki.org/wiki/API:Search #
             #################################################################################################
 
-            url = "https://en.wikipedia.org/w/api.php"
+            # url = "https://en.wikipedia.org/w/api.php"
+            #
+            # payload = {
+            #     "action": "query",
+            #     "format": "json",
+            #     "list": "search",
+            #     "srsearch": route  # search for pages containing 'route'
+            # }
+            #
+            # response = requests.get(url=url, params=payload)
+            #
+            # if response.json()['query']['search'][0]['title'] == route:
+            #     replacement = route.replace(" ", "_")
+            #     my_url += replacement
+            #     print("done replacement: ", my_url)
 
-            payload = {
-                "action": "query",
-                "format": "json",
-                "list": "search",
-                "srsearch": route  # search for pages containing 'route'
-            }
-
-            response = requests.get(url=url, params=payload)
-
-            if response.json()['query']['search'][0]['title'] == route:
-                replacement = route.replace(" ", "_")
-                my_url += replacement
+            replacement = route.replace(" ", "_")
+            my_url += replacement
 
         elif method == "url":
             my_url = route
 
+        # print(my_url)
         result = requests.get(my_url)
         package = result.content
 
         soup = BeautifulSoup(package, "html.parser")
         # find all paragraphs under the div element that has this specific ID
+
+        # print(soup)
+
+        lang = soup.find(id="bodyContent").find_all("span")
         body = soup.find(id="bodyContent").find_all("p")
+
+        for x in lang:
+            string = str(x)
+            # print(type(string), string)
+            if "<span lang" in string:
+                foreign_lang = True
+                break
+
         target_paragraph = 0
 
         for x in body:
@@ -52,9 +70,18 @@ def scraper(route, method):
         final_text = first_paragraph.find('p').get_text()  # text is now a string
 
 
+
         target = "\[[^[]*\]"
         final_text = re.sub(target, "", final_text)
         final_text = re.sub("\n", "", final_text)
+        final_text = re.sub(u"\u00A0", "", final_text)
+        final_text = re.sub(u"\u2013", "-", final_text)
+
+        target = ""
+
+        if foreign_lang:
+            target = "\([^.]*\)"
+            final_text = re.sub(target, "", final_text)
 
 
         data = {"title": route, "content": final_text}
@@ -85,7 +112,14 @@ def get_data(keyword):
 # so user must format the url to be: wikipedia.org/<endpoint>
 # Can Alex help format actual urls into this format to use my service?
 @app.route('/get_url/<string:domain>/<string:endpoint>')
-def get_data1(domain, endpoint):
+def get_data2(domain, endpoint):
     url = "http://en." + domain + "/wiki/" + endpoint
+    json_data = scraper(url, "url")
+    return json_data
+
+# if user wants a random wiki article
+@app.route('/get_random')
+def get_data1():
+    url = "https://en.wikipedia.org/wiki/Special:Random"
     json_data = scraper(url, "url")
     return json_data
